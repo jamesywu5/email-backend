d6c7632fd3cdb6eb4c99e1c7d56f92515e29b77b
James Wu <jamesywu5@gmail.com>
2026-06-20 14:38:10 -0700

    spring initializr setup made

---
+++ src/main/java/com/jameswu/email_backend/EmailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jameswu.email_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jameswu/email_backend/EmailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameswu.email_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
